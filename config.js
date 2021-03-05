715f6e438720ed85b4f112cfc2480b1acaa24dc6
*The-HellCat <61914405+TheHellCat0@users.noreply.github.com>
2021-03-05 18:36:06 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,52 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+
+//-------------------
+
+//-------------------
+const config = {
+  ownerTag: "*HellCat#8191",
+  ownerID: "437187116003491841",
+  botID: "669179772575940629",
+  
+  //------------
+  dblapi:  "",
+  discordbotlistapi : "",
+  bfdapi : "",
+  dboats : "",
+  giphyapi: "",
+  tenorapi: "",
+  YOUTUBE_APİ_KEY: "",
+  prefix: "s.",
+  token: ""
+  
+/*
+                                                                                               
+   SSSSSSSSSSSSSSS hhhhhhh                               kkkkkkkk                             
+ SS:::::::::::::::Sh:::::h                               k::::::k                             
+S:::::SSSSSS::::::Sh:::::h                               k::::::k                             
+S:::::S     SSSSSSSh:::::h                               k::::::k                             
+S:::::S             h::::h hhhhh       uuuuuu    uuuuuu   k:::::k    kkkkkkk  aaaaaaaaaaaaa   
+S:::::S             h::::hh:::::hhh    u::::u    u::::u   k:::::k   k:::::k   a::::::::::::a  
+ S::::SSSS          h::::::::::::::hh  u::::u    u::::u   k:::::k  k:::::k    aaaaaaaaa:::::a 
+  SS::::::SSSSS     h:::::::hhh::::::h u::::u    u::::u   k:::::k k:::::k              a::::a 
+    SSS::::::::SS   h::::::h   h::::::hu::::u    u::::u   k::::::k:::::k        aaaaaaa:::::a 
+       SSSSSS::::S  h:::::h     h:::::hu::::u    u::::u   k:::::::::::k       aa::::::::::::a 
+            S:::::S h:::::h     h:::::hu::::u    u::::u   k:::::::::::k      a::::aaaa::::::a 
+            S:::::S h:::::h     h:::::hu:::::uuuu:::::u   k::::::k:::::k    a::::a    a:::::a 
+SSSSSSS     S:::::S h:::::h     h:::::hu:::::::::::::::uuk::::::k k:::::k   a::::a    a:::::a 
+S::::::SSSSSS:::::S h:::::h     h:::::h u:::::::::::::::uk::::::k  k:::::k  a:::::aaaa::::::a 
+S:::::::::::::::SS  h:::::h     h:::::h  uu::::::::uu:::uk::::::k   k:::::k  a::::::::::aa:::a
+ SSSSSSSSSSSSSSS    hhhhhhh     hhhhhhh    uuuuuuuu  uuuukkkkkkkk    kkkkkkk  aaaaaaaaaa  aaaa
+ */                                                                
+ 
+
+
+
+  
+  
+
+
+};
+
+module.exports = config
